fcf2cf3bf2eafea327497e824e0de9b6c16b8435
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-15 00:46:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	var ans='';
+
+	for(var i=0;i<s.length;i++){
+		if(s[i]===' '){
+			if(ans===''){
+				continue;
+			}else
+			break;
+		}
+		ans+=s[i];
+	}
+	return ans;
 }
 
 // Do not change the code below
